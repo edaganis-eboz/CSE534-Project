704b406f61b60f89b7edb70c5cfd496b987e2566
Erin Ozcan <ebozcan@asu.edu>
2024-10-14 12:34:37 -0700

    init commit for key-gen

--- client/client.py
+++ client/client.py
@@ -1,8 +1,16 @@
 # This will be the clients. This *should* be written entirely in python
 
 class Client():
-    def __init__(self) -> None:
+    
+    def __init__(self):
         pass
+
+    def generate_key(self):
+        pass
+
+    def load_key(self):
+        pass
+
 
 
 
