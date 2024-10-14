fb56dbb2e4b489d4ba443bffe0b3ab806c548d0b
Erin Ozcan <ebozcan@asu.edu>
2024-10-14 10:56:40 -0700

    Added start for client

---
+++ client/client.py
@@ -0,0 +1,13 @@
+# This will be the clients. This *should* be written entirely in python
+
+class Client():
+    def __init__(self) -> None:
+        pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
