b9f15a4d900c96f45fc031893d2cecfaee1832fa
Erin Ozcan <ebozcan@asu.edu>
2024-11-21 16:09:41 -0700

    fixed l4 enc

--- L4_enc/client.py
+++ L4_enc/client.py
@@ -7,12 +7,11 @@ def main():
     cipher = AES.new(key, AES.MODE_CBC)
     server_addr = ('127.0.0.1', 1337)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('', 12345))
         s.connect(server_addr)
-        p = input()
         message = str(time.time()).encode()
         ciphertext = cipher.encrypt(pad(message, 16))
-        s.sendall(ciphertext)
+        s.sendall(cipher.iv + ciphertext)
+        s.close()
 
 
 
--- L4_enc/server.py
+++ L4_enc/server.py
@@ -4,7 +4,7 @@ from Crypto.Util.Padding import unpad
 import time
 def main():
     key = b'AAAAAAAAAAAAAAAA'
-    cipher = AES.new(key, AES.MODE_CBC)
+    
     ip = "127.0.0.1"
     port = 1337
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -13,13 +13,16 @@ def main():
 
         while True:
             client_socket, client_addr = s.accept()
-            print(f'Connected to {client_addr}')
+            # print(f'Connected to {client_addr}')
             try:
-                data = s.recv(1024)
+                data = client_socket.recv(1024)
                 if data:
-                    plaintext = float(unpad(cipher.decrypt(data),16))
+                    iv = data[:16]
+                    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+                    plaintext = float(unpad(cipher.decrypt(data[16:]),16))
                     print(f"One way ping: {time.time() - plaintext}")
             except KeyboardInterrupt:
+                s.close()
                 break
             except Exception as e:
                 print(f"{e}")
